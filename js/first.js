94903413d035872c96a34866728578f2f57b4fc4
Golfhouse <1002713302@qq.com>
2016-12-06 22:52:40 +0800

    medical

---
+++ js/first.js
@@ -0,0 +1,15 @@
+(function(win,doc){
+		function change(){
+			doc.documentElement.style.fontSize=40*doc.documentElement.clientWidth/320+'px';
+		}
+		change();
+		window.addEventListener('resize',change,false);
+	})(window,document);
+	window.onload=function(){
+		$("#menu_ico").click(function(){
+			$("#menu").css("display","block");
+		});
+		$("#box_1,#box_2,#box_3").click(function(){
+			$("#menu").css("display","none");
+		});
+	};
